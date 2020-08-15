51bce037db4a1568db142769a2125c23aff54f0d
Ricardo Canelas <ricardocanelas@gmail.com>
2020-08-15 18:30:44 -0300

    feat: update app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ModalProvider, useModal } from "./hooks/useModal";
+import ToDoModal from "./modals/TodoModal";
+import ConfirmDialog from "./modals/ConfirmDialog";
+
+const List = () => {
+  const { isShow, open } = useModal();
+
+  return (
+    <div>
+      {isShow("ToDo") ? "showing toDo" : "not showing toDo"} <br />
+      {isShow("ConfirmDialog")
+        ? "showing ConfirmDialog"
+        : "not showing ConfirmDialog"}
+      <hr />
+      <button onClick={() => open("ToDo")} className="mr-3">
+        Open To Do Modal
+      </button>
+      <button onClick={() => open("ConfirmDialog")}>Open ConfirmDialog</button>
+      <ToDoModal />
+      <ConfirmDialog />
+    </div>
+  );
+};
+
+// --- APP ---
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ModalProvider>
+      <List />
+    </ModalProvider>
   );
 }
 
