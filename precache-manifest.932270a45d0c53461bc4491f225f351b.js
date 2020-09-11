3ecd9a000f82cfeb3eeb90b008797aac7b56f457
Ricardo Canelas <ricardocanelas@gmail.com>
2020-09-11 08:36:23 -0300

    Updates

---
+++ precache-manifest.932270a45d0c53461bc4491f225f351b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63db67f47d65cf2c9df2227ea9d5996f",
+    "url": "/useModal-example/index.html"
+  },
+  {
+    "revision": "14315821cacd6b8bade8",
+    "url": "/useModal-example/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d4c0012d67126dc4de29",
+    "url": "/useModal-example/static/css/main.5d0807bf.chunk.css"
+  },
+  {
+    "revision": "14315821cacd6b8bade8",
+    "url": "/useModal-example/static/js/2.24739abe.chunk.js"
+  },
+  {
+    "revision": "5e731a9c54d7793793f7c86255e71979",
+    "url": "/useModal-example/static/js/2.24739abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c0012d67126dc4de29",
+    "url": "/useModal-example/static/js/main.9bf68d2e.chunk.js"
+  },
+  {
+    "revision": "6ea55e341155ee857517",
+    "url": "/useModal-example/static/js/runtime-main.1ac8b194.js"
+  }
+]);
